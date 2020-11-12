49ac434c2b0753f9772693c02a7e9018ddd33fc2
Margui <margui.berjon@gmail.com>
2020-11-12 15:39:00 +0100

    try form

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -16,6 +16,17 @@ function Contact() {
           Enviar
         </button>
       </form>
+      <form name="contact" method="POST" data-netlify="true" subject='porfolio'>
+        <div className='form-name-email-container'>
+          <input type='text' name='name' placeholder='Nombre' className='input-form' />
+          <input type='text' name='email' placeholder='Email' className='input-form' />
+        </div>
+        <textarea name='message' placeholder='Mensaje' className='input-form text-area'></textarea>
+        <button type='submit' value='Submit'>
+          Enviar
+        </button>
+      </form>
+
       <div className='contact-logos'>
         <a href='https://www.linkedin.com/in/margaritaberjon/' target='_blank' rel='noopener noreferrer'>
           <img src={Linkedin} alt='Linkedin'></img>

10edeb1ef6fab89941ad79982cdd949836fc29a7
Margui <margui.berjon@gmail.com>
2020-11-18 13:08:44 +0100

    Add validations to form

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,20 +1,47 @@
-import React from 'react';
+import React , { useState }  from 'react';
 import './Contact.scss';
 import Linkedin from '../../images/linkedin.svg';
 import GitHub from '../../images/github.svg';
 
 function Contact() {
+  const [form, setForm] =
+  useState({
+    name: 'Nombre',
+    email: 'Email',
+    message: 'Mensaje',
+  });
+const [inputRequired, setInputRequired] = useState({name:'',email:'',message:''});
+const [disable, setDisable] = useState(true);
+
+
+
+  function handleChange(ev) {
+    setDisable(ev.target.value === '');
+    setForm(ev.target.value)
+    const name = ev.target.name;
+    const value = ev.target.value;
+    setInputRequired(old=> {
+      let newValue = Object.assign({},old)
+      newValue[name] = value.length ? '' : 'input-required '
+      return newValue
+    })
+  }
+
+
+ 
+
   return (
     <>
-      <form action='https://formspree.io/f/mnqozngk' method='POST' className='form-container'>
+      <form action='https://formspree.io/f/mnqozngk' method='POST' className='form-container' onChange={handleChange} >
         <div className='form-name-email-container'>
-          <input type='text' name='name' placeholder='Nombre' className='input-form' />
-          <input type='text' name='email' placeholder='Email' className='input-form' />
+          <input type='text' name='name' placeholder={form.name} className={inputRequired.name ? inputRequired.name+'input-form' : 'input-form'} required/>
+          <input type='text' name='email' placeholder='Email' className={inputRequired.name ? inputRequired.name+'input-form' : 'input-form'} required />
         </div>
-        <textarea name='message' placeholder='Mensaje' className='input-form text-area'></textarea>
-        <button type='submit' value='Submit'>
+        <textarea name='message' placeholder='Mensaje' className={inputRequired.message ? inputRequired.message+'input-form text-area' : 'input-form text-area'} required></textarea>
+        <button disabled={disable} type='submit' value='Submit'>
           Enviar
         </button>
+       
       </form>
       <div className='contact-logos'>
         <a href='https://www.linkedin.com/in/margaritaberjon/' target='_blank' rel='noopener noreferrer'>
